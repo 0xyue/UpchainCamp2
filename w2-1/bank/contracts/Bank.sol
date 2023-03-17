c6149aa32d7debce3851a384bd160540ea451d51
0xyue <0xyue.eth@gmail.com>
2023-03-17 19:59:47 +0800

    w2-1 update

--- w2-1/bank/contracts/Bank.sol
+++ w2-1/bank/contracts/Bank.sol
@@ -2,10 +2,25 @@
 pragma solidity ^0.8.10;
 
 contract Bank {
+    address public owner;
     mapping(address => uint256) private balances;
 
     event Deposit(address indexed account, uint256 amount);
     event Withdrawal(address indexed account, uint256 amount);
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender == owner,"not owner");
+        _;
+    }
+
+    receive() external payable {
+        balances[msg.sender] += msg.value;
+        emit Deposit(msg.sender, msg.value);
+    }
 
     function deposit() public payable {
         require(msg.value > 0, "Amount must be greater than zero");
@@ -20,6 +35,13 @@ contract Bank {
         (bool success,) = payable(msg.sender).call{value:balance}("");
         require(success, "Withdrawal failed");
         emit Withdrawal(msg.sender, balance);
+    }
+
+    function withdrawAll() public onlyOwner {
+        uint b = address(this).balance;
+        (bool success,) = payable(owner).call{value:b}("");
+        require(success, "WithdrawAll failed");
+        emit Withdrawal(msg.sender, b);
     }
 
     function getBalance(address account) public view returns (uint256) {
